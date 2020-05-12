df547f14e03d1e43e7c87c82d8a118aa3b225ca0
andar <andar@syneforge.com>
2020-05-12 09:20:59 +0300

    Initial commit

---
+++ precache-manifest.33e2699bd6a7dcbd50ff4c9b5118618e.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550a0d17cc728166f2013a939b5f942a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b4a0a310ea4322d2e9f4",
+    "url": "/static/css/2.7ffcbf6c.chunk.css"
+  },
+  {
+    "revision": "2a76b8e77cadc4b89795",
+    "url": "/static/css/main.6c270760.chunk.css"
+  },
+  {
+    "revision": "b4a0a310ea4322d2e9f4",
+    "url": "/static/js/2.cc327ec6.chunk.js"
+  },
+  {
+    "revision": "6a295aff43d23cb433faa4b8362c1bc3",
+    "url": "/static/js/2.cc327ec6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2a76b8e77cadc4b89795",
+    "url": "/static/js/main.6aa40877.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  },
+  {
+    "revision": "709b92da34810382cee2e32735b85793",
+    "url": "/static/media/Kurale-Regular.709b92da.woff2"
+  },
+  {
+    "revision": "7d51b22df8d50b7f3cee3a038ef45bac",
+    "url": "/static/media/Kurale-Regular.7d51b22d.woff"
+  },
+  {
+    "revision": "6a258acda8a5b8f041cc9efd3a7b480a",
+    "url": "/static/media/OpenSans-Regular.6a258acd.woff"
+  },
+  {
+    "revision": "8abbb9d98c0c7304060190592408ab78",
+    "url": "/static/media/OpenSans-Regular.8abbb9d9.woff2"
+  },
+  {
+    "revision": "2595cae6483bb50cbeb0cb40e3292231",
+    "url": "/static/media/OpenSans-SemiBold.2595cae6.woff2"
+  },
+  {
+    "revision": "a53cea7f3118cf93d173424cf70ec545",
+    "url": "/static/media/OpenSans-SemiBold.a53cea7f.woff"
+  },
+  {
+    "revision": "2bc126808b43f83fe1326d0061d34111",
+    "url": "/static/media/Pacifico-Regular.2bc12680.woff"
+  },
+  {
+    "revision": "e6435d1f9880b48b120230bbee41365a",
+    "url": "/static/media/Pacifico-Regular.e6435d1f.woff2"
+  }
+]);
